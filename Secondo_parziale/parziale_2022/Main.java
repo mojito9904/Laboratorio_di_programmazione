301ca29d0f6f4356a4b89153b2a93550119b9f0e
mojito9904 <angeloceck0006@gmail.com>
2025-11-26 18:17:25 +0100

    Aggiunti i primi due metodi del parziale datato 2022

--- Secondo_parziale/parziale_2022/Main.java
+++ Secondo_parziale/parziale_2022/Main.java
@@ -2,9 +2,18 @@ package Laboratorio_di_programmazione.Secondo_parziale.parziale_2022;
 
 public class Main {
     public static void main(String args[]){
-        String [][]a = {{"adb","dt","dir"},{"jr","jgt"},{"fx","f","gif","fy"}};
+        /*main per metodo 1 */
+        /*String [][]a = {{"adb","dt","dir"},{"jr","jgt"},{"fx","f","gif","fy"}};
         char[] c = {'d','j','f'};
         boolean []b=metodo.metodo1(a, c);
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }*/
+       /*main metodo2 */
+       String [] []a= {{"ops","t","cr7","yr"},{"qr","y5","g"},{"FM3","k","zk"}};
+        int []v = {2,3,2};
+        int k = 2;
+        String[]b=metodo.metodo2(a, v, k);
         for (int i = 0; i < b.length; i++) {
             System.out.println(b[i]);
         }
--- Secondo_parziale/parziale_2022/metodo.java
+++ Secondo_parziale/parziale_2022/metodo.java
@@ -30,4 +30,42 @@ public class metodo{
         }
         return b;
     }
+
+    /*
+    Scrivere un metodo statico iterativo che, dati un array bidimensionale di
+    stringhe a, un array monodimensionale di interi v ed un intero k, restituisce un array mo-
+    nodimensionale di stringhe b tale che b[i] `e la stringa ottenuta concatenando le prime v[i]
+    stringhe nella riga a[i] (letta da sx a dx) la cui lunghezza `e maggiore o uguale a k. Se in a[i]
+    non esistono tali v[i] stringhe, allora b[i] `e la stringa "**".
+    Esempio: se a = {{"ops","t","cr7","yr"},{"qr","y5","g"},{"FM3","k","zk"}},
+    v = {2,3,2}e k = 2, il metodo restituisce l’array b = {"opscr7","**","FM3zk"}.
+    Si assuma che le lunghezze di a e v siano uguali e che i valori di v e di k siano strettamente
+    positivi. N.B. Occorre passare alla riga successiva non appena si trovano in a[i] le v[i]
+    stringhe che soddisfano la condizione richiesta.
+     */
+    public static String [] metodo2(String[][] a,int[]v,int k){
+        String[] b=new String[a.length];
+        String s;
+        int j;
+        int cont;
+        for(int i=0;i<a.length;i++){
+            j=0;
+            cont=0;
+            s="";
+            while(j<a[i].length && cont<v[i] ){
+                if(a[i][j].length()>=k){
+                     s+=a[i][j];
+                     cont++;
+                }
+                j++;
+            }
+            if(cont<v[i]){
+                b[i]="**";
+            }
+            else {
+                b[i]=s;
+            }
+        }
+        return b;
+    }
 }
