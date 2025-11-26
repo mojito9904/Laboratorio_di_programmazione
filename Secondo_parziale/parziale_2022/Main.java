67b8c57dd364cae8bd070172ad55fd6bbbfa2127
mojito9904 <angeloceck0006@gmail.com>
2025-11-26 21:39:21 +0100

    aggiunti i file del parziale 2022 con i commenti

--- Secondo_parziale/parziale_2022/Main.java
+++ Secondo_parziale/parziale_2022/Main.java
@@ -1,21 +1,107 @@
 package Laboratorio_di_programmazione.Secondo_parziale.parziale_2022;
 
 public class Main {
-    public static void main(String args[]){
-        /*main per metodo 1 */
-        /*String [][]a = {{"adb","dt","dir"},{"jr","jgt"},{"fx","f","gif","fy"}};
-        char[] c = {'d','j','f'};
-        boolean []b=metodo.metodo1(a, c);
-        for (int i = 0; i < b.length; i++) {
-            System.out.println(b[i]);
-        }*/
-       /*main metodo2 */
-       String [] []a= {{"ops","t","cr7","yr"},{"qr","y5","g"},{"FM3","k","zk"}};
-        int []v = {2,3,2};
-        int k = 2;
-        String[]b=metodo.metodo2(a, v, k);
-        for (int i = 0; i < b.length; i++) {
-            System.out.println(b[i]);
+    public static void main(String args[]) {
+
+        /* ============================================================
+         *                     MAIN PER METODO 1
+         * ============================================================
+         * Test del metodo metodo1():
+         * - Verifica se tutte le stringhe di ogni riga iniziano con il
+         *   carattere corrispondente nel vettore c.
+         *
+         * Esempio commentato:
+         *
+         * String [][] a = {
+         *     {"adb", "dt", "dir"},
+         *     {"jr",  "jgt"},
+         *     {"fx",  "f", "gif", "fy"}
+         * };
+         *
+         * char[] c = {'d', 'j', 'f'};
+         *
+         * boolean [] b = metodo.metodo1(a, c);
+         *
+         * for (int i = 0; i < b.length; i++) {
+         *     System.out.println(b[i]);   // stampa true/false per ogni riga
+         * }
+         */
+
+
+        /* ============================================================
+         *                     MAIN PER METODO 2 (ITERATIVO)
+         * ============================================================
+         * Test del metodo metodo2():
+         * - Ogni elemento di b è la concatenazione delle prime v[i]
+         *   stringhe lunghe almeno k presenti nella riga a[i].
+         * - Se tali stringhe NON esistono, b[i] diventa "**".
+         *
+         * Esempio commentato:
+         *
+         * String[][] a = {
+         *     {"ops", "t", "cr7", "yr"},
+         *     {"qr", "y5", "g"},
+         *     {"FM3", "k", "zk"}
+         * };
+         *
+         * int[] v = {2, 3, 2};
+         * int k = 2;
+         *
+         * String[] b = metodo.metodo2(a, v, k);
+         *
+         * for (int i = 0; i < b.length; i++) {
+         *     System.out.println(b[i]);
+         * }
+         */
+
+        /* ============================================================
+         *                  MAIN PER METODO 2 (RICORSIVO)
+         * ============================================================
+         * Stesso test di sopra, ma usando la versione ricorsiva:
+         *
+         * String[][] a = {
+         *     {"ops", "t", "cr7", "yr"},
+         *     {"qr", "y5", "g"},
+         *     {"FM3", "k", "zk"}
+         * };
+         *
+         * int[] v = {2, 3, 2};
+         * int k = 2;
+         *
+         * String[] b = metodo.metodo2_ricorsivo(a, v, k);
+         *
+         * for (int i = 0; i < b.length; i++) {
+         *     System.out.println(b[i]);
+         * }
+         */
+
+
+        /* ============================================================
+         *                     MAIN PER METODO 3
+         * ============================================================
+         * Test del metodo metodo3():
+         * - Restituisce una matrice di caratteri dove:
+         *      'y' = l’elemento è divisibile per b[i]
+         *      'n' = altrimenti
+         */
+        /* 
+        int[][] a = {
+            {12, -1},
+            {-7, 9, 3, 8},
+            {5, 10, 3}
+        };
+
+        int[] b = {4, 3, 2};
+
+        char[][] c = metodo.metodo3(a, b);
+
+        // Stampa della matrice risultante
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < c[i].length; j++) {
+                System.out.println(c[i][j]);
+            }
+            System.out.println(); // Riga vuota per separare le righe della matrice
         }
+        */
     }
 }
--- Secondo_parziale/parziale_2022/metodo.java
+++ Secondo_parziale/parziale_2022/metodo.java
@@ -1,71 +1,178 @@
 package Laboratorio_di_programmazione.Secondo_parziale.parziale_2022;
-public class metodo{
+
+public class metodo {
+
     /*
-    Scrivere un metodo statico iterativo che, dati un array bidimensionale di
-    stringhe a ed un array monodimensionale di caratteri c, restituisce un array monodimen-
-    sionale di booleani b tale che b[i] vale true se tutte le stringhe nella riga a[i] hanno c[i] come
-    primo carattere, altrimenti b[i] vale false.
-    Esempio: se a = {{"adb","dt","dir"},{"jr","jgt"},{"fx","f","gif","fy"}}e
-    c = {’d’,’j’,’f’}, il metodo restituisce l’array b = {false,true,false}.
-    Si assuma che le lunghezze di a e c siano uguali e che ogni stringa in a sia non vuota.
-    N.B. Occorre passare alla riga successiva non appena si trova in a[i] la prima stringa che
-    non soddisfa la condizione data
+     * Metodo 1
+     * Scrivere un metodo statico iterativo che, dati un array bidimensionale di
+     * stringhe a ed un array monodimensionale di caratteri c, restituisce un array 
+     * monodimensionale di booleani b tale che:
+     * 
+     *  b[i] = true  se TUTTE le stringhe nella riga a[i] hanno c[i] come primo carattere
+     *  b[i] = false altrimenti
+     *
+     * Appena si trova una stringa che NON soddisfa la condizione, si passa alla riga successiva.
+     *
+     * Esempio:
+     *  a = {{"adb","dt","dir"},{"jr","jgt"},{"fx","f","gif","fy"}}
+     *  c = {'d','j','f'}
+     *  Risultato: b = {false, true, false}
+     *
+     * Si assume che:
+     *  - le lunghezze di a e c siano uguali
+     *  - ogni stringa in a sia non vuota
      */
-    public static boolean [] metodo1(String[][] a, char[]c){
-        boolean[] b= new boolean[a.length];
+    public static boolean[] metodo1(String[][] a, char[] c) {
+        boolean[] b = new boolean[a.length];
         boolean controllo;
+
         for (int i = 0; i < a.length; i++) {
-            controllo=true;
-            int j=0;
-            while(j<a[i].length && controllo ){
-                if(!(a[i][j].charAt(0) ==c[i])) {
-                    controllo=false;
-                }
-                else{
+            controllo = true; // ipotizzo che tutte vadano bene
+            int j = 0;
+
+            // Scorro la riga finché non trovo un errore
+            while (j < a[i].length && controllo) {
+                if (a[i][j].charAt(0) != c[i]) {
+                    controllo = false; // prima stringa non valida → passo alla riga successiva
+                } else {
                     j++;
                 }
-                 
             }
-            b[i]=controllo;
+            b[i] = controllo;
         }
         return b;
     }
 
     /*
-    Scrivere un metodo statico iterativo che, dati un array bidimensionale di
-    stringhe a, un array monodimensionale di interi v ed un intero k, restituisce un array mo-
-    nodimensionale di stringhe b tale che b[i] `e la stringa ottenuta concatenando le prime v[i]
-    stringhe nella riga a[i] (letta da sx a dx) la cui lunghezza `e maggiore o uguale a k. Se in a[i]
-    non esistono tali v[i] stringhe, allora b[i] `e la stringa "**".
-    Esempio: se a = {{"ops","t","cr7","yr"},{"qr","y5","g"},{"FM3","k","zk"}},
-    v = {2,3,2}e k = 2, il metodo restituisce l’array b = {"opscr7","**","FM3zk"}.
-    Si assuma che le lunghezze di a e v siano uguali e che i valori di v e di k siano strettamente
-    positivi. N.B. Occorre passare alla riga successiva non appena si trovano in a[i] le v[i]
-    stringhe che soddisfano la condizione richiesta.
+     * Metodo 2 (iterativo)
+     * Scrivere un metodo statico iterativo che, dati:
+     *   - un array bidimensionale di stringhe a
+     *   - un array di interi v
+     *   - un intero k
+     *
+     * restituisce un array di stringhe b tale che:
+     *
+     *  b[i] = concatenazione delle prime v[i] stringhe di a[i]
+     *         aventi lunghezza >= k
+     *
+     *  Se nella riga a[i] NON esistono almeno v[i] stringhe di lunghezza >= k,
+     *  allora b[i] = "**".
+     *
+     * Appena si trovano le v[i] stringhe buone, si passa alla riga successiva.
+     *
+     * Esempio:
+     *  a = {{"ops","t","cr7","yr"}, {"qr","y5","g"}, {"FM3","k","zk"}}
+     *  v = {2,3,2}
+     *  k = 2
+     *  Risultato: {"opscr7", "**", "FM3zk"}
+     *
+     * Si assume che:
+     *  - a.length == v.length
+     *  - v[i] > 0 e k > 0
      */
-    public static String [] metodo2(String[][] a,int[]v,int k){
-        String[] b=new String[a.length];
+    public static String[] metodo2(String[][] a, int[] v, int k) {
+        String[] b = new String[a.length];
         String s;
         int j;
         int cont;
-        for(int i=0;i<a.length;i++){
-            j=0;
-            cont=0;
-            s="";
-            while(j<a[i].length && cont<v[i] ){
-                if(a[i][j].length()>=k){
-                     s+=a[i][j];
-                     cont++;
+
+        for (int i = 0; i < a.length; i++) {
+            j = 0;
+            cont = 0;
+            s = "";
+
+            // Cerco le prime v[i] stringhe con lunghezza >= k
+            while (j < a[i].length && cont < v[i]) {
+                if (a[i][j].length() >= k) {
+                    s += a[i][j];
+                    cont++;
                 }
                 j++;
             }
-            if(cont<v[i]){
-                b[i]="**";
-            }
-            else {
-                b[i]=s;
+
+            // Se non ne ho trovate abbastanza → "**"
+            if (cont < v[i]) {
+                b[i] = "**";
+            } else {
+                b[i] = s;
             }
         }
         return b;
+    }
+
+    /*
+     * Metodo 2 (ricorsivo)
+     * Versione ricorsiva del metodo precedente.
+     */
+    public static String[] metodo2_ricorsivo(String[][] a, int[] v, int k) {
+        String[] b = new String[a.length];
+        return metodo2_ricorsivo(a, v, k, b, 0, 0, 0, "");
+    }
+
+    public static String[] metodo2_ricorsivo(
+        String[][] a, int[] v, int k,
+        String[] b, int i, int j, int cont, String s
+    ) {
+        // Caso base: ho completato tutte le righe
+        if (i == a.length) return b;
+
+        // Caso in cui ho finito la riga o ho trovato abbastanza stringhe
+        if (j == a[i].length || cont == v[i]) {
+            if (cont < v[i]) {
+                b[i] = "**";
+            } else {
+                b[i] = s;
+            }
+            // Passo alla riga successiva
+            return metodo2_ricorsivo(a, v, k, b, i + 1, 0, 0, "");
+        }
+
+        // Caso ricorsivo: controllo la stringa corrente
+        if (a[i][j].length() >= k) {
+            s += a[i][j];
+            cont++;
+        }
+
+        return metodo2_ricorsivo(a, v, k, b, i, j + 1, cont, s);
+    }
+
+    /*
+     * Metodo 3
+     * Scrivere un metodo statico iterativo che, dati:
+     *
+     *  - un array bidimensionale di interi a
+     *  - un array monodimensionale di interi b
+     *
+     * restituisce un array bidimensionale di caratteri c tale che:
+     *
+     *  c[i][j] = 'y' se a[i][j] è divisibile per b[i]
+     *  c[i][j] = 'n' altrimenti
+     *
+     * Esempio:
+     *  a = {{12,-1}, {-7,9,3,8}, {5,10,3}}
+     *  b = {4,3,2}
+     *
+     * Risultato:
+     *  c = {{'y','n'}, {'n','y','y','n'}, {'n','y','n'}}
+     *
+     * Si assume:
+     *  - a.length == b.length
+     */
+    public static char[][] metodo3(int[][] a, int[] b) {
+        char[][] c = new char[a.length][];
+
+        for (int i = 0; i < a.length; i++) {
+            c[i] = new char[a[i].length];
+
+            for (int j = 0; j < a[i].length; j++) {
+                if (a[i][j] % b[i] == 0) {
+                    c[i][j] = 'y';
+                } else {
+                    c[i][j] = 'n';
+                }
+            }
+        }
+
+        return c;
     }
 }
