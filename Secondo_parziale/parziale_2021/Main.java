458a6c48770e822c84b15102ab61ebbdb68d2a6f
mojito9904 <angeloceck0006@gmail.com>
2025-11-26 10:38:59 +0100

    inseriti i primi due esercizi

--- Secondo_parziale/parziale_2021/Main.java
+++ Secondo_parziale/parziale_2021/Main.java
@@ -2,6 +2,20 @@ package Laboratorio_di_programmazione.Secondo_parziale.parziale_2021;
 
 public class Main {
     public static void main(String arg[]){
-        
+        /*Primo main per il primo metodo */
+        /* 
+        String [][]a = {{"abc","bb","d"},{"rkr","rc"},{"mp","aqsfg","ats","ar"}};
+        char[] c = {'b','r','a'}; 
+        int k = 4;
+        System.out.println(metodo.metodo1(a, c, k));
+        */
+        /*Main per il metodo 2 con e senza ricorsione */
+        String []a = {"apqpwp","gwwg","jxxh","y","qq","rzrr"};
+        char[]c = {'p','g','x','z','q','r'};
+        //String[]b=metodo.metodo2(a, c);
+        String[] b= metodo.metodo2_ricorsivo(a, c);
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
     }
 }
--- Secondo_parziale/parziale_2021/metodo.java
+++ Secondo_parziale/parziale_2021/metodo.java
@@ -14,5 +14,67 @@ public class metodo {
     soddisfa la condizione data.
     ii) Occorre restituire false non appena si trova la prima riga a[i] che non soddisfa la condizione
     richiesta.
-     */   
+     */
+    public static boolean metodo1(String[][] a,char[] c,int k){
+        for (int i = 0; i < a.length; i++) {
+            int j=0;
+            boolean found=false;
+            while(j<a[i].length && !found){
+                if(a[i][j].length()<k && a[i][j].charAt(0)==c[i]){
+                    found=true;
+                }
+                else{
+                    j++;
+                }
+            }
+            if(!found){
+                return false;
+            }
+        }
+        return true;
+    }  
+    /*
+    Scrivere un metodo statico iterativo che, dati un array monodimensionale di
+    stringhe a ed un array monodimensionale di caratteri c, restituisce un array monodimensio-
+    nale di stringhe b tale che b[i] `e la stringa ottenuta da a[i] senza le occorrenze del carattere
+    c[i].
+    Esempio: se a = {"apqpwp","gwwg","jxxh","y","qq","rzrr"}e c = {’p’,’g’,’x’,’z’,
+    ’q’,’r’}, il metodo restituisce l’array b = {"aqw","ww","jh","y","","z"}.
+    Si assuma che gli array a e c abbiano lunghezza uguale.
+     */
+    public static String[] metodo2(String[]a,char[]c){
+        String[] b=new String[a.length];
+        String s;
+        for (int i = 0; i < a.length; i++) {
+            int j=0;
+            s="";
+            while(j<a[i].length()){
+                if(!(a[i].charAt(j)==c[i])){
+                    s+=a[i].charAt(j);
+                }
+                j++;
+            }
+            b[i]=s;
+        }
+        return b;
+    }
+
+    /*metodo ricorsivo */
+    public static String[] metodo2_ricorsivo(String[]a,char[]c){
+        String[] b=new String[a.length];
+        return metodo2_ricorsivo(a, c,b,0,0,"");
+    }
+    
+    public static String[] metodo2_ricorsivo(String[]a, char[]c,String[] b, int i,int j,String s){
+        if(i==a.length) return b;
+        if(j==a[i].length()) {
+            b[i]=s;
+            return metodo2_ricorsivo(a, c, b, i+1, 0, "");
+        }
+        if(!(a[i].charAt(j)==c[i])){
+            s+=a[i].charAt(j);
+            return metodo2_ricorsivo(a, c, b, i, j+1, s);
+        }
+        return metodo2_ricorsivo(a, c, b, i, j+1, s);
+    }
 }
