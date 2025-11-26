e05ca57a0c7c624a6fb9e70ef6346b8b3093078a
mojito9904 <angeloceck0006@gmail.com>
2025-11-26 10:05:01 +0100

    Creati i file del parziale del 2021

---
+++ Secondo_parziale/parziale_2021/Main.java
@@ -0,0 +1,7 @@
+package Laboratorio_di_programmazione.Secondo_parziale.parziale_2021;
+
+public class Main {
+    public static void main(String arg[]){
+        
+    }
+}
---
+++ Secondo_parziale/parziale_2021/metodo.java
@@ -0,0 +1,18 @@
+package Laboratorio_di_programmazione.Secondo_parziale.parziale_2021;
+
+public class metodo {
+    /*
+    Scrivere un metodo statico iterativo che, dati un array bidimensionale di
+    stringhe a, un array monodimensionale di caratteri c ed un intero k, restituisce true se in
+    ogni riga a[i] esiste almeno una stringa il cui primo carattere `e uguale a c[i] e la cui lunghezza
+    `e minore di k; altrimenti il metodo restituisce false.
+    Esempio: se a = {{"abc","bb","d"},{"rkr","rc"},{"mp","aqsfg","ats","ar"}},
+    c = {’b’,’r’,’a’}e k = 4, il metodo restituisce true.
+    Si assuma che gli array a e c abbiano lunghezza uguale, che ogni stringa in a sia non vuota
+    e k > 0.
+    N.B. i) Occorre passare alla riga successiva non appena si trova in a[i] la prima stringa che
+    soddisfa la condizione data.
+    ii) Occorre restituire false non appena si trova la prima riga a[i] che non soddisfa la condizione
+    richiesta.
+     */   
+}
