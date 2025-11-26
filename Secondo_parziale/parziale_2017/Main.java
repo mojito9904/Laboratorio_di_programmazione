3c85f6bc73e298403657e7e337e892d533a411b3
mojito9904 <angeloceck0006@gmail.com>
2025-11-26 15:28:51 +0100

    Aggiunti i commenti ai codici del parziale del 2017

--- Secondo_parziale/parziale_2017/Main.java
+++ Secondo_parziale/parziale_2017/Main.java
@@ -2,47 +2,79 @@ package Laboratorio_di_programmazione.Secondo_parziale.parziale_2017;
 
 public class Main {
     public static void main(String arg[]){
-        /*Main per il primo metodo senza ricorsione */
-        /* 
-        String [][]a = {{"attf","jr","brw","acxf"},{"","ytd","lp"},{"z","abcd","rt"}};
+
+        /* ============================================================
+           MAIN PER IL PRIMO METODO (versione iterativa, non ricorsiva)
+           ============================================================ */
+        /*
+        // Array bidimensionale di stringhe
+        String [][] a = {
+            {"attf","jr","brw","acxf"},
+            {"","ytd","lp"},
+            {"z","abcd","rt"}
+        };
+        // Array dei limiti di lunghezza
         int[] v = {3,4,2};
-        String [] b =metodo.metodo1(a, v);
+        // Chiamata del metodo
+        String [] b = metodo.metodo1(a, v);
+        // Stampa del risultato
         for (int i = 0; i < b.length; i++) {
             System.out.println(b[i]);
         }
         */
-       /*Main per il primo metodo con ricorsione */
-        /* 
-        String [][]a = {{"attf","jr","brw","acxf"},{"","ytd","lp"},{"z","abcd","rt"}};
+
+        /* ============================================================
+           MAIN PER IL PRIMO METODO (versione ricorsiva)
+           ============================================================ */
+        /*
+        String [][] a = {
+            {"attf","jr","brw","acxf"},
+            {"","ytd","lp"},
+            {"z","abcd","rt"}
+        };
         int[] v = {3,4,2};
-        String [] b =metodo.metodo1_ricorsivo(a, v);
+        String [] b = metodo.metodo1_ricorsivo(a, v);
         for (int i = 0; i < b.length; i++) {
             System.out.println(b[i]);
         }
         */
-       /*Main per il secondo metodo senza ricorsione */
-       /* 
-        String[] a= {"abbcd","ad","bffgh","jswf","frs"};
-        char[] c={'b','h','f','w','a'};
-        int k=2;
+
+        /* ============================================================
+           MAIN PER IL SECONDO METODO (versione iterativa)
+           ============================================================ */
+        /*
+        String[] a = {"abbcd","ad","bffgh","jswf","frs"};
+        char[] c   = {'b','h','f','w','a'};
+        int k = 2;
+        // Stampa true se ogni stringa ha almeno k caratteri != c[i]
         System.out.println(metodo.metodo2(a, c, k));
         */
-       /*Metodo main per secodno metodo ma con ricorsione */
-        /*String[] a= {"abbcd","ad","bffgh","jswf","frs"};
-        char[] c={'b','h','f','w','a'};
-        int k=2;
-        System.out.println(metodo.metodo2(a, c, k));
+
+        /* ============================================================
+           MAIN PER IL SECONDO METODO (versione ricorsiva)
+           ============================================================ */
+        /*
+        String[] a = {"abbcd","ad","bffgh","jswf","frs"};
+        char[] c   = {'b','h','f','w','a'};
+        int k = 2;
+        // Stessa logica del metodo sopra, ma implementata ricorsivamente
+        System.out.println(metodo.metodo2_ricorsivo(a, c, k));
         */
-       /*Main per il terzo metodo  */
-       /* 
-       int []a={5,-4,-1,3};
-       int[][]b=metodo.metodo3(a);
-       for (int i = 0; i < b.length; i++) {
-        for (int j = 0; j < b[i].length; j++) {
-            System.out.print(b[i][j]+"");
+
+        /* ============================================================
+           MAIN PER IL TERZO METODO
+           ============================================================ */
+        /*
+        int[] a = {5, -4, -1, 3};
+        int[][] b = metodo.metodo3(a);
+        // Stampa il contenuto della matrice risultato
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println(); // Va a capo dopo ogni riga
         }
-        System.out.println();
-       }
-       */
+        */
+
     }
 }
--- Secondo_parziale/parziale_2017/metodo.java
+++ Secondo_parziale/parziale_2017/metodo.java
@@ -1,115 +1,170 @@
 package Laboratorio_di_programmazione.Secondo_parziale.parziale_2017;
 
 public class metodo {
+
     /* Scrivere un metodo che, dati un array bidimensionale di stringhe a ed un array
-    monodimensionale di interi v, restituisce un array monodimensionale di stringhe b tale che b[i]
-    e la concatenazione delle stringhe in a[i] (lette da sx a dx) la cui lunghezza e maggiore di v[i].
-    Esempio: se a = "attf","jr","brw","acxf","","ytd","lp","z","abcd","rt"
-    e v = 3,4,2 , il metodo restituisce b = "attfacxf","","abcd" .
-    Si assuma che gli array a e v abbiano la stessa lunghezza. */
+       monodimensionale di interi v, restituisce un array monodimensionale di stringhe b tale che b[i]
+       è la concatenazione delle stringhe in a[i] (lette da sx a dx) la cui lunghezza è maggiore di v[i].
+       Esempio: se a = {"attf","jr","brw","acxf","","ytd","lp","z","abcd","rt"}
+       e v = {3,4,2}  , il metodo restituisce b = {"attfacxf","","abcd"}.
+       Si assume che a e v abbiano la stessa lunghezza.
+    */
     public static String[] metodo1(String [][]a, int []v){
-        String [] b=new String [a.length];
-        for(int i=0;i<a.length;i++){
-            String s="";
-            for (int j = 0; j <a[i].length; j++) {
-                if(a[i][j].length()>v[i]){
-                    s+=a[i][j];
+        // Array risultato con una stringa per ogni riga di 'a'
+        String [] b = new String[a.length];
+
+        for(int i = 0; i < a.length; i++){
+            String s = "";  // Accumulatore delle stringhe valide nella riga i
+
+            for (int j = 0; j < a[i].length; j++) {
+                // Aggiunge la stringa solo se la lunghezza è > v[i]
+                if(a[i][j].length() > v[i]){
+                    s += a[i][j];
                 }
             }
-            b[i]=s;
+
+            b[i] = s;   // Inserisce la concatenazione nella posizione corretta
         }
         return b;
     }
 
-    /*Metodo 1 ma con la ricorsioni */
+    /* Metodo 1 ma ricorsivo */
     public static String [] metodo1_ricorsivo(String [][]a,int []v){
-        String [] b=new String[a.length];
-        return metodo1_ricorsivo(a, v,b,0,0,"");
+        // Array risultato da riempire ricorsivamente
+        String [] b = new String[a.length];
+        return metodo1_ricorsivo(a, v, b, 0, 0, "");
     }
-    public static String[] metodo1_ricorsivo(String [][]a, int[] v,String[] b,int i,int j,String s){
-        if(i==a.length) return b;
-        if(j==a[i].length){
-            b[i]=s;
+
+    // Versione ricorsiva che scorre (i = riga, j = colonna)
+    public static String[] metodo1_ricorsivo(String [][]a, int[] v, String[] b,
+                                            int i, int j, String s){
+
+        // Caso base: terminate tutte le righe → ritorna il risultato
+        if(i == a.length) return b;
+
+        // Se abbiamo terminato la riga corrente
+        if(j == a[i].length){
+            b[i] = s;  // Salva la stringa accumulata
+            // Passa alla riga successiva (reset j e s)
             return metodo1_ricorsivo(a, v, b, i+1, 0, "");
         }
-        if(a[i][j].length()>v[i]){
-            s+=a[i][j];
-            return metodo1_ricorsivo(a, v, b, i, j+1, s);
+
+        // Se la stringa corrente va concatenata
+        if(a[i][j].length() > v[i]){
+            s += a[i][j]; // Aggiunge
         }
+
+        // Continua sulla stessa riga incrementando j
         return metodo1_ricorsivo(a, v, b, i, j+1, s);
     }
 
     /*
-    Scrivere un metodo che, dati un array monodimensionale di stringhe a, un
-    array monodimensionale di caratteri c ed un intero k, restituisce true se in ogni stringa a[i] di
-    a esistono almeno k caratteri diversi da c[i], altrimenti il metodo restituisce false. Esempio:
-    se a = "abbcd","ad","bffgh","jswf","frs" , c = b,h,f,w,a e k = 2, il
-    metodo restituisce true.
-    N.B. i) Occorre restituire false non appena si trova una stringa a[i] che non soddisfa la
-    condizione richiesta. ii) Occorre passare alla stringa successiva non appena si veri ca che
-    una stringa a[i] soddisfa la condizione richiesta. iii) Si assuma che gli array a e c abbiano la
-    stessa lunghezza e k>0.
-     */
-    public static boolean metodo2(String []a,char[] c,int k){
-        int cont;
+       Scrivere un metodo che, dati un array di stringhe a, un array di caratteri c e un intero k,
+       restituisce true se in ogni stringa a[i] esistono almeno k caratteri diversi da c[i].
+       Altrimenti restituisce false.
+
+       Note:
+       - Restituire false appena una stringa non soddisfa la condizione.
+       - Appena una stringa soddisfa la condizione si passa alla successiva.
+    */
+    public static boolean metodo2(String []a, char[] c, int k){
+        int cont; // Contatore dei caratteri diversi da c[i]
         int j;
+
         for (int i = 0; i < a.length; i++) {
-            j=0;
-            cont=0;
-            while(j<a[i].length() && cont<k){
-                if(a[i].charAt(j)!=c[i]){
+            j = 0;
+            cont = 0;
+
+            // Scorre la stringa finché non raggiunge k o la fine
+            while(j < a[i].length() && cont < k){
+                if(a[i].charAt(j) != c[i]){
                     cont++;
                 }
                 j++;
             }
-            if(cont<k){
+
+            // Se cont < k allora la stringa a[i] non soddisfa la condizione
+            if(cont < k){
                 return false;
             }
         }
-        return true;
+
+        return true; // Tutte le stringhe soddisfano la condizione
     }
 
-    /*metodo precedente ma ricorsivo */
-    public static boolean metodo2_ricorsivo(String []a,char[] c,int k){
-        return metodo2_ricorsivo(a,c,k,0,0,0);
+    /* Metodo precedente ma ricorsivo */
+    public static boolean metodo2_ricorsivo(String []a, char[] c, int k){
+        return metodo2_ricorsivo(a, c, k, 0, 0, 0);
     }
-    public static boolean metodo2_ricorsivo(String []a,char[] c,int k,int i, int j, int cont){
-        if (i==a.length) return true;
-        if(j==a[i].length()){
-            if(cont>=k){
+
+    // i = indice della stringa, j = posizione nella stringa, cont = contatore caratteri != c[i]
+    public static boolean metodo2_ricorsivo(String []a, char[] c, int k,
+                                            int i, int j, int cont){
+
+        // Sono state controllate tutte le stringhe
+        if (i == a.length) return true;
+
+        // Se siamo arrivati alla fine della stringa a[i]
+        if(j == a[i].length()){
+            // Se soddisfa la condizione passa alla successiva
+            if(cont >= k){
                 return metodo2_ricorsivo(a, c, k, i+1, 0, 0);
             }
             else{
-                return false;
-            } 
-        }
-        if(a[i].charAt(j)!=c[i]){
-                cont++;
-                return metodo2_ricorsivo(a, c, k, i,j+1,cont);
+                return false; // Non soddisfa la condizione → fallimento immediato
             }
+        }
+
+        // Se il carattere è diverso da c[i], incrementa cont
+        if(a[i].charAt(j) != c[i]){
+            cont++;
+        }
+
+        // Prosegue con il carattere successivo
         return metodo2_ricorsivo(a, c, k, i, j+1, cont);
     }
 
-    /*Scrivere un metodo che, dato un array monodimensionale di interi a, restituisce 
-    un array bidimensionale di interi b tale che la riga b[i] contiene i numeri da 0 ad a[i] 
-    se a[i] >=0, mentre b[i] contiene i numeri da a[i] a 0 se a[i]<0. Esempio: se a = 5,-4,-1,3 ,
-    il metodo restituisce l'array b = 0,1,2,3,4,5,-4,-3,-2,-1,0,-1,0,0,1,2,3  */
+    /*
+       Scrivere un metodo che, dato un array di interi a, restituisce un array bidimensionale b
+       tale che:
+
+       - Se a[i] >= 0   ⇒ b[i] contiene i numeri da 0 a a[i]
+       - Se a[i] <  0   ⇒ b[i] contiene i numeri da a[i] a 0
+
+       Esempio:
+       a = {5, -4, -1, 3}
+       b = {0,1,2,3,4,5},
+           {-4,-3,-2,-1,0},
+           {-1,0},
+           {0,1,2,3}
+    */
     public static int [][] metodo3(int[] a) {
-    int[][] b=new int[a.length][];
-    for (int i=0;i<a.length;i++) {
-        if (a[i]>=0) {
-            b[i]=new int[a[i]+1]; 
-            for (int j=0;j<=a[i];j++) {
-                b[i][j]=j;
-            }
-        } else {
-            b[i]=new int[-a[i]+1]; 
-            for (int j=0;j<= -a[i];j++) {
-                b[i][j]=a[i]+j;
+
+        int[][] b = new int[a.length][];
+
+        for (int i = 0; i < a.length; i++) {
+
+            if (a[i] >= 0) {
+                // Se positivo: lunghezza = a[i] + 1 (incluso)
+                b[i] = new int[a[i] + 1];
+
+                // Riempie da 0 a a[i]
+                for (int j = 0; j <= a[i]; j++) {
+                    b[i][j] = j;
+                }
+
+            } else {
+                // Se negativo: lunghezza = |a[i]| + 1
+                b[i] = new int[-a[i] + 1];
+
+                // Riempie da a[i] a 0
+                for (int j = 0; j <= -a[i]; j++) {
+                    b[i][j] = a[i] + j;
+                }
             }
         }
-    }
-    return b;
-    }
 
+        return b;
+    }
 }
+
