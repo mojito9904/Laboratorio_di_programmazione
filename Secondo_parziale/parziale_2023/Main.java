f70a2523bd0e002fdffc2518715063101def49b1
mojito9904 <angeloceck0006@gmail.com>
2025-12-02 16:10:41 +0100

    Inseriti i commenti del parziale 2023

--- Secondo_parziale/parziale_2023/Main.java
+++ Secondo_parziale/parziale_2023/Main.java
@@ -1,27 +1,126 @@
 package Laboratorio_di_programmazione.Secondo_parziale.parziale_2023;
 
 public class Main {
-    public  static void main(String args[]){
-        
-        /*String[][] a = {{"bk","x","jrw"},{"h5mvc","qkw","fp","z"},{"t3qw","zq"}};
+    public static void main(String args[]) {
+
+        /* ============================================================
+         *                     MAIN PER METODO 1
+         * ============================================================
+         * Test del metodo metodo1():
+         * - Controlla se esiste almeno una riga dell’array bidimensionale
+         *   le cui stringhe, lette da sinistra verso destra, hanno
+         *   lunghezze strettamente decrescenti.
+         *
+         * Esempio commentato:
+         *
+         * String[][] a = {
+         *     {"bk", "x", "jrw"},
+         *     {"h5mvc", "qkw", "fp", "z"},
+         *     {"t3qw", "zq"}
+         * };
+         *
+         * System.out.println(metodo.metodo1(a));
+         */
+
+        String[][] a = {
+            {"bk", "x", "jrw"},
+            {"h5mvc", "qkw", "fp", "z"},
+            {"t3qw", "zq"}
+        };
         System.out.println(metodo.metodo1(a));
-        */
-        /*String [] a = {"cd","ab","ghr"}; 
-        String[] c = {"ghr","ab","yrt","ab","cd","ghr","ab"};
-        int k = 2; //il metodo restituisce l’array b = {false,true,true}.
-        boolean []b=metodo.metodo2_ricorsivo(a,c,k);
-        for (int i = 0; i < b.length; i++) {
-            System.out.println(b[i]);
+
+
+        /* ============================================================
+         *                     MAIN PER METODO 2 (ITERATIVO)
+         * ============================================================
+         * Test del metodo metodo2():
+         * - b[i] vale true se la stringa a[i] compare almeno k volte
+         *   all’interno dell’array c.
+         * - Altrimenti b[i] vale false.
+         *
+         * Esempio commentato:
+         *
+         * String[] a = {"cd", "ab", "ghr"};
+         * String[] c = {"ghr", "ab", "yrt", "ab", "cd", "ghr", "ab"};
+         * int k = 2;
+         *
+         * boolean[] b = metodo.metodo2(a, c, k);
+         *
+         * for (int i = 0; i < b.length; i++)
+         *     System.out.println(b[i]);
+         */
+        /*
+        String[] a2 = {"cd", "ab", "ghr"};
+        String[] c2 = {"ghr", "ab", "yrt", "ab", "cd", "ghr", "ab"};
+        int k = 2;
+
+        boolean[] b2 = metodo.metodo2(a2, c2, k);
+
+        for (int i = 0; i < b2.length; i++) {
+            System.out.println(b2[i]);
         }
         */
-        String[] a = {"bncz","as","rvc"};
+
+
+        /* ============================================================
+         *                MAIN PER METODO 2 (RICORSIVO)
+         * ============================================================
+         * Versione ricorsiva dello stesso esercizio del metodo 2.
+         *
+         * String[] a = {"cd", "ab", "ghr"};
+         * String[] c = {"ghr", "ab", "yrt", "ab", "cd", "ghr", "ab"};
+         * int k = 2;
+         *
+         * boolean[] b = metodo.metodo2_ricorsivo(a, c, k);
+         *
+         * for (int i = 0; i < b.length; i++)
+         *     System.out.println(b[i]);
+         */
+        /*
+        String[] a3 = {"cd", "ab", "ghr"};
+        String[] c3 = {"ghr", "ab", "yrt", "ab", "cd", "ghr", "ab"};
+        int k3 = 2;
+
+        boolean[] b3 = metodo.metodo2_ricorsivo(a3, c3, k3);
+
+        for (int i = 0; i < b3.length; i++) {
+            System.out.println(b3[i]);
+        }
+        */
+
+
+        /* ============================================================
+         *                     MAIN PER METODO 3
+         * ============================================================
+         * Test del metodo metodo3():
+         * - Per ogni stringa dell’array a, crea una riga di nuove stringhe,
+         *   ognuna ottenuta concatenando ogni singolo carattere di a[i]
+         *   con la stringa s.
+         *
+         * Esempio commentato:
+         *
+         * String[] a = {"bncz", "as", "rvc"};
+         * String s = "ulla";
+         *
+         * Risultato atteso:
+         * {
+         *     {"bulla", "nulla", "culla", "zulla"},
+         *     {"aulla", "sulla"},
+         *     {"rulla", "vulla", "culla"}
+         * }
+         */
+        /*
+        String[] a4 = {"bncz", "as", "rvc"};
         String s = "ulla";
-        String [][]b=metodo.metodo3(a, s);
-        for (int i = 0; i < b.length; i++) {
-            for (int j = 0; j < b[i].length; j++) {
-                System.out.print(b[i][j]+" ");
+
+        String[][] b4 = metodo.metodo3(a4, s);
+
+        for (int i = 0; i < b4.length; i++) {
+            for (int j = 0; j < b4[i].length; j++) {
+                System.out.print(b4[i][j] + " ");
             }
             System.out.println();
         }
+        */
     }
 }
--- Secondo_parziale/parziale_2023/metodo.java
+++ Secondo_parziale/parziale_2023/metodo.java
@@ -3,111 +3,178 @@ package Laboratorio_di_programmazione.Secondo_parziale.parziale_2023;
 public class metodo {
 
     /*
-   Scrivere un metodo statico iterativo che, dato un array bidimensionale di
-stringhe a, restituisce true se esiste almeno una riga a[i] tale che le stringhe in a[i] (letta da
-sx a dx) hanno lunghezze strettamente decrescenti, altrimenti il metodo restituisce false.
-Esempio: se a = {{"bk","x","jrw"},{"h5mvc","qkw","fp","z"},{"t3qw","zq"}},
-il metodo restituisce true.
-N.B. i) Occorre passare alla riga successiva non appena si verifica che le stringhe in a[i] non
-soddisfano la condizione richiesta. ii) Occorre restituire true non appena si trova la prima
-riga a[i] che soddisfa la condizione richiesta. 
+     * ===============================================================
+     * ESERCIZIO 1
+     * ---------------------------------------------------------------
+     * Scrivere un metodo statico iterativo che, dato un array
+     * bidimensionale di stringhe a, restituisce true se esiste almeno
+     * una riga a[i] le cui stringhe (da sinistra a destra) hanno
+     * lunghezze strettamente decrescenti.
+     *
+     * Esempio:
+     * a = {
+     *   {"bk","x","jrw"},
+     *   {"h5mvc","qkw","fp","z"},
+     *   {"t3qw","zq"}
+     * }
+     * Il metodo restituisce true perché la riga 0 rispetta la condizione.
+     *
+     * NOTE:
+     * - Appena una coppia nella riga non rispetta la condizione → si passa
+     *   alla riga successiva.
+     * - Appena si trova una riga valida → si restituisce true.
+     * ===============================================================
      */
+    public static boolean metodo1(String [][] a){
+        boolean trovato = true;
 
-    public static boolean metodo1(String [][]a){
-        boolean trovato=true;
-        for (int i=0;i<a.length;i++){
-            trovato=true;
-            int j=0;
-            while(j<a[i].length-1 && trovato){
-                if(!(a[i][j].length()>a[i][j+1].length())){
-                    trovato=false;
+        // scorre ogni riga della matrice
+        for (int i = 0; i < a.length; i++){
+            trovato = true; // supponiamo sia valida la riga corrente
+
+            int j = 0;
+            // controlliamo le lunghezze a coppie consecutive
+            while (j < a[i].length - 1 && trovato){
+                if (!(a[i][j].length() > a[i][j+1].length())) {
+                    trovato = false; // condizione violata
                 }
                 j++;
             }
-            if(trovato) return trovato;
+
+            if (trovato) return true; // riga valida → true
         }
-        return trovato;
-    }  
+
+        return false; // nessuna riga valida
+    }
 
 
-    /* 
-       Esercizio 2. Scrivere un metodo statico iterativo che, dati due array monodimensionali di
-    stringhe a e c ed un intero k, restituisce un array monodimensionale di booleani b tale che
-    b[i] vale true se la stringa a[i] compare almeno k volte in c, altrimenti b[i] vale false.
-    Esempio: se a = {"cd","ab","ghr"}, c = {"ghr","ab","yrt","ab","cd","ghr","ab"}e
-    k = 2, il metodo restituisce l’array b = {false,true,true}.
-    Si assuma che il valore di k sia strettamente positivo.
-    N.B. Occorre passare alla stringa successiva non appena si verifica che a[i] soddisfa la con-
-    dizione richiesta.
-    */
 
-
-    public static boolean [] metodo2 (String [] a, String[]c, int k){
-        boolean [] b= new boolean [a.length];
+    /*
+     * ===============================================================
+     * ESERCIZIO 2 (ITERATIVO)
+     * ---------------------------------------------------------------
+     * Dati due array di stringhe a e c e un intero k, restituisce un
+     * array di booleani b dove:
+     *
+     * b[i] = true  se a[i] compare almeno k volte in c
+     * b[i] = false altrimenti
+     *
+     * Esempio:
+     * a = {"cd","ab","ghr"}
+     * c = {"ghr","ab","yrt","ab","cd","ghr","ab"}
+     * k = 2
+     *
+     * Risultato: b = {false, true, true}
+     *
+     * - La ricerca di ciascun elemento di a si ferma non appena
+     *   compare k volte.
+     * ===============================================================
+     */
+    public static boolean[] metodo2(String[] a, String[] c, int k){
+        boolean[] b = new boolean[a.length];
         int cont;
         int j;
-        for (int i=0;i<a.length;i++){
-            cont=0;
-            j=0;
-            while(j<c.length && cont<k){
-                if(a[i].equals(c[j])){
+
+        for (int i = 0; i < a.length; i++){
+            cont = 0;
+            j = 0;
+
+            // scorro c finché non arrivo alla fine OPPURE cont raggiunge k
+            while (j < c.length && cont < k){
+                if (a[i].equals(c[j])){
                     cont++;
                 }
                 j++;
             }
-            if(cont==k){
-                b[i]=true;
-            }
-            else{
-                b[i]=false;
-            }
+
+            b[i] = (cont == k); // true se ha raggiunto k occorrenze
         }
+
         return b;
     }
 
 
-    public static boolean[] metodo2_ricorsivo (String[]a,String[]c,int k){
-        boolean b[]=new boolean[a.length];
-        return metodo2_ricorsivo(a, c, k,b,0,0,0);
-    }
-    public static boolean[] metodo2_ricorsivo(String[] a,String[]c,int k, boolean[]b ,int i,int j,int cont){
-        if(i==a.length) return b;
-        if(j==c.length || cont==k){
-            if (cont==k){
-                b[i]=true;
-                return metodo2_ricorsivo(a, c, k, b, i+1, 0, 0);
-            }
-            else{
-                b[i]=false;
-                return metodo2_ricorsivo(a, c, k, b, i+1, 0, 0);
-            }
-        }
-        if(a[i].equals(c[i])) cont++;
 
+    /*
+     * ===============================================================
+     * ESERCIZIO 2 (RICORSIVO)
+     * ---------------------------------------------------------------
+     * Versione ricorsiva dell’esercizio precedente.
+     * ===============================================================
+     */
+    public static boolean[] metodo2_ricorsivo(String[] a, String[] c, int k){
+        boolean[] b = new boolean[a.length];
+        return metodo2_ricorsivo(a, c, k, b, 0, 0, 0);
+    }
+
+    /*
+     * Parametri ricorsivi:
+     * - b : array dei risultati
+     * - i : indice dell'array a (elemento corrente di a da cercare)
+     * - j : indice dell'array c (posizione corrente nella ricerca)
+     * - cont : conteggio delle occorrenze trovate per a[i]
+     */
+    public static boolean[] metodo2_ricorsivo(String[] a, String[] c, int k,
+                                              boolean[] b, int i, int j, int cont){
+
+        // caso base: abbiamo elaborato tutte le stringhe di a
+        if (i == a.length) return b;
+
+        // se ho finito c oppure ho già trovato k occorrenze
+        if (j == c.length || cont == k){
+
+            // imposto il risultato per a[i]
+            b[i] = (cont == k);
+
+            // passo al prossimo elemento di a
+            return metodo2_ricorsivo(a, c, k, b, i+1, 0, 0);
+        }
+
+        // CORRETTO: devo confrontare a[i] con c[j], non c[i]
+        if (a[i].equals(c[j])) cont++;
+
+        // continua a scorrere c
         return metodo2_ricorsivo(a, c, k, b, i, j+1, cont);
     }
+
+
+
     /*
-    Esercizio 4. Scrivere un metodo statico iterativo che, dati un array monodimensionale di
-    stringhe a ed una stringa s, restituisce un array bidimensionale di stringhe b tale che la riga
-    b[i] contiene le stringhe ottenute concatenando i caratteri della stringa a[i] (letta da sx a dx)
-    con la stringa s.
-    Esempio: se a = {"bncz","as","rvc"}ed s = "ulla", il metodo restituisce l’array
-    b = {{"bulla","nulla","culla","zulla"},{"aulla","sulla"},{"rulla","vulla",
-    "*/
+     * ===============================================================
+     * ESERCIZIO 4
+     * ---------------------------------------------------------------
+     * Dato un array a e una stringa s, restituisce una matrice b tale
+     * che b[i] contiene tante stringhe quanti sono i caratteri di a[i].
+     *
+     * Ogni stringa è ottenuta prendendo il carattere a[i].charAt(j)
+     * e concatenandolo con s.
+     *
+     * Esempio:
+     * a = {"bncz","as","rvc"}
+     * s = "ulla"
+     *
+     * Risultato:
+     * b = {
+     *   {"bulla","nulla","culla","zulla"},
+     *   {"aulla","sulla"},
+     *   {"rulla","vulla","culla"}
+     * }
+     * ===============================================================
+     */
     public static String[][] metodo3(String[] a, String s){
-        String[][]b=new String[a.length][];
-        for (int i = 0; i < a.length; i++) {
-            b[i]=new String[a[i].length()];
-            for (int j = 0; j < a[i].length(); j++) {
-                char c=a[i].charAt(j);
-                b[i][j]=c+s;
+        String[][] b = new String[a.length][];
+
+        // per ogni stringa di a
+        for (int i = 0; i < a.length; i++){
+            b[i] = new String[a[i].length()]; // una cella per ogni carattere
+
+            for (int j = 0; j < a[i].length(); j++){
+                char c = a[i].charAt(j); // estraggo il singolo carattere
+                b[i][j] = c + s;         // concateno il carattere con s
             }
         }
+
         return b;
     }
 
 }
-
-
-
-    
